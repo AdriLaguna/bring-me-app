6b2f70af84b7fcfc0ee2a73470c40283904d2709
AdriLaguna <adrilagunam@gmail.com>
2022-01-03 20:48:42 +0100

    React added

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -14,20 +14,20 @@ export const Users = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const res = await fetch(`${API}/user`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            name,
-            email,
-            password,
-            address,
-            zip_code,
-            phone,
-            admin
-        })
-    })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+        address,
+        zip_code,
+        phone,
+        admin,
+      }),
+    });
     const data = await res.json();
     console.log(data);
   };
@@ -39,58 +39,63 @@ export const Users = () => {
           <div className="form-group">
             <input
               type="text"
-              onChange={e => setName(e.target.value)}
+              onChange={(e) => setName(e.target.value)}
               value={name}
               className="form-control"
               placeholder="Nombre"
               autoFocus
-            /><br />
+            />
+            <br />
             <input
               type="email"
-              onChange={e => setEmail(e.target.value)}
+              onChange={(e) => setEmail(e.target.value)}
               value={email}
               className="form-control"
               placeholder="Email"
-            /><br />
+            />
+            <br />
             <input
               type="password"
-              onChange={e => setPassword(e.target.value)}
+              onChange={(e) => setPassword(e.target.value)}
               value={password}
               className="form-control"
               placeholder="Contraseña"
-            /><br />
+            />
+            <br />
             <input
               type="text"
-              onChange={e => setAddress(e.target.value)}
+              onChange={(e) => setAddress(e.target.value)}
               value={address}
               className="form-control"
               placeholder="Dirección"
-            /><br />
+            />
+            <br />
             <input
               type="text"
-              onChange={e => setZipCode(e.target.value)}
+              onChange={(e) => setZipCode(e.target.value)}
               value={zip_code}
               className="form-control"
               placeholder="Código Postal"
-            /><br />
+            />
+            <br />
             <input
               type="text"
-              onChange={e => setPhone(e.target.value)}
+              onChange={(e) => setPhone(e.target.value)}
               value={phone}
               className="form-control"
               placeholder="Teléfono"
-            /><br />
+            />
+            <br />
             <input
               type="text"
-              onChange={e => setAdmin(e.target.value)}
+              onChange={(e) => setAdmin(e.target.value)}
               value={admin}
               className="form-control"
               placeholder="Admin"
-            /><br />
+            />
+            <br />
           </div>
-          <button className="btn btn-primary btn-block">
-              Registrar
-          </button>
+          <button className="btn btn-primary btn-block">Registrar</button>
         </form>
       </div>
       <div className="col md-8"></div>
