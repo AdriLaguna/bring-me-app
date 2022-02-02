bb1e346af284ecc9c517199b702b12cc09ecab44
themanofsteel1976 <dptohux@uma.es>
2022-02-02 08:23:02 +0100

    mapa datos abiertos

--- frontend/src/components/MapView.js
+++ frontend/src/components/MapView.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer, Tooltip } from "react-leaflet";
+import { Icon } from "leaflet";
+import useSwr from "swr";
 import "leaflet/dist/leaflet.css";
 import "../App.css";
 
@@ -8,16 +10,46 @@ import Markers from './Markers'
 
 const API = process.env.REACT_APP_API;
 
+const fetcher = (...args) => fetch(...args).then(response => response.json());
+
+const icon = new Icon({
+  iconUrl: "/biofuel-svgrepo-com.svg",
+  iconSize: [25, 25]
+});
+
 const MapView = () => {
-    const {gasolineras} = fetch(`${API}/gasolineras-biodiesel`)
+
+  const url = `${API}/gasolineras-biodiesel`
+  const {data, error} = useSwr(url, fetcher);
+  const gasolineras = data && !error ? data : [];
+  const [activeGasolinera, setActiveGasolinera] = useState(null);
+  //const {gasolineras} = fetch(`${API}/gasolineras-biodiesel`)
+  
   return (
-    <MapContainer center={{ lat: "40.1498916", lng: "-4.8803695" }} zoom={6}>
+    <MapContainer center={{ lat: "36.7202" , lng: "-4.4203"  }} zoom={8}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       ></TileLayer>
-      <Markers places={gasolineras}></Markers>
-    </MapContainer>
+      
+      {gasolineras.map(gasolinera => (
+        <Marker key={gasolinera.IDEESS}
+                position={[gasolinera['Latitud'].replace(",","."), gasolinera['Longitud (WGS84)'].replace(",",".")]}
+                icon={icon}
+                
+        >
+           <Tooltip direction='top' opacity={1} >
+
+            <p> Estación: {gasolinera.IDEESS} ({gasolinera.Rótulo}) </p>
+            <p> {gasolinera.Localidad} ({gasolinera.Provincia}) </p>
+            <p> Precio Biodiesel: {gasolinera['Precio Biodiesel']} </p>
+            
+          </Tooltip>
+
+        </Marker>  
+      ))}
+
+     </MapContainer>
   );
 };
 
